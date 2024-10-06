e37de6f5e163c0e42d862e53d613595fe41e07a5
Han Chen <hellohanchen@gmail.com>
2024-10-06 00:42:06 -0700

    current nba plays only

--- provider/topshot/moments/m7_extract_nba_data.py
+++ provider/topshot/moments/m7_extract_nba_data.py
@@ -3,7 +3,10 @@ import os
 import pathlib
 
 
-def extract_vgn_data():
+def extract_nba_data():
+    with open(os.path.join(pathlib.Path(__file__).parent.resolve(), "../../nba/data/current_nba_players.json"), 'r') as player_file:
+        nba_players = json.load(player_file)
+
     with open(os.path.join(pathlib.Path(__file__).parent.resolve(), "resource/enriched_plays.json"),
               'r') as plays_file:
         loaded = json.load(plays_file)
@@ -11,6 +14,12 @@ def extract_vgn_data():
         result = {}
 
         for play_id in loaded['plays']:
+            play_1 = loaded['plays'][play_id][0]
+            if play_1['playerId'] is not None and str(play_1['playerId']) not in nba_players:
+                continue
+            if play_1['playerId'] is None and play_1['teamId'] == 0:
+                continue
+
             result[play_id] = {}
             for play in loaded['plays'][play_id]:
                 result[play_id][play['setFlowId']] = {
@@ -21,10 +30,10 @@ def extract_vgn_data():
                     'teamId': play['teamId'],
                 }
 
-        with open(os.path.join(pathlib.Path(__file__).parent.resolve(), "resource/vgn_plays.json"),
-                  'w') as vgn_file:
-            json.dump(result, vgn_file, indent=2)
+    with open(os.path.join(pathlib.Path(__file__).parent.resolve(), "resource/nba_plays.json"),
+              'w') as vgn_file:
+        json.dump(result, vgn_file, indent=2)
 
 
 if __name__ == '__main__':
-    extract_vgn_data()
+    extract_nba_data()
--- provider/topshot/moments/main.py
+++ provider/topshot/moments/main.py
@@ -2,11 +2,11 @@ from provider.topshot.moments.m2_load_graphql_plays import load_set_plays
 from provider.topshot.moments.m4_enrich_play_badges import enrich_plays
 from provider.topshot.moments.m5_group_by_player import group_play_by_player
 from provider.topshot.moments.m6_checklists import group_play_to_checklists
-from provider.topshot.moments.m7_extract_vgn_data import extract_vgn_data
+from provider.topshot.moments.m7_extract_nba_data import extract_nba_data
 
 if __name__ == '__main__':
     load_set_plays()
     enrich_plays()
     group_play_by_player()
     group_play_to_checklists()
-    extract_vgn_data()
+    extract_nba_data()
